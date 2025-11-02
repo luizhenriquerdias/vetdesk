9b27269ec10f537951f0653be7054a9ee45247b7
Luiz Henrique Dias <luizhenriquerdias@gmail.com>
2025-11-02 18:54:49 -0300

    add login page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,14 +1,34 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 import InternalLayout from '@/layouts/internal/index.vue';
+import ExternalLayout from '@/layouts/external/index.vue';
 import HomePage from '@/pages/home/index.vue';
+import LoginPage from '@/pages/login/index.vue';
+import { ROUTE_LOGIN, ROUTE_HOME } from './routes';
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes: [
-    { path: '/',
+    {
+      path: '/',
+      component: ExternalLayout,
+      children: [
+        {
+          name: ROUTE_LOGIN,
+          path: 'login',
+          component: LoginPage,
+        },
+      ],
+    },
+    {
+      path: '/',
       component: InternalLayout,
       children: [
-        { path: '/', component: HomePage },
-      ] },
+        {
+          name: ROUTE_HOME,
+          path: '',
+          component: HomePage,
+        },
+      ],
+    },
   ],
 });
---
+++ frontend/src/router/routes.ts
@@ -0,0 +1,2 @@
+export const ROUTE_LOGIN = 'login' as const;
+export const ROUTE_HOME = 'home' as const;
