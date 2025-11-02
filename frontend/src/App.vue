44b0e85920d5e58dfd3a03844b9326c6d3d8d848
Luiz Henrique Dias <luizhenriquerdias@gmail.com>
2025-11-02 17:33:45 -0300

    Refactor code style and structure in Vue components and configuration files
    - Updated `vite.config.ts` to use consistent semicolon usage.
    - Refactored `App.vue` and `HelloWorld.vue` templates for improved readability by formatting HTML attributes and adding missing semicolons in script sections.
    - Ensured consistent import statements across files in `main.ts` and `HelloWorld.vue`.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a
+      href="https://vite.dev"
+      target="_blank"
+    >
+      <img
+        src="/vite.svg"
+        class="logo"
+        alt="Vite logo"
+      >
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+    >
+      <img
+        src="./assets/vue.svg"
+        class="logo vue"
+        alt="Vue logo"
+      >
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -28,3 +38,7 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
