c7b949cc4dd819441ecd921697db17e06044e2f9
Luiz Henrique Dias <luizhenriquerdias@gmail.com>
2025-11-09 15:56:49 -0300

    Update dependencies and implement PostgreSQL session management
    - Added `connect-pg-simple` for session management with PostgreSQL.
    - Updated `pg` to version 8.16.3 for improved database interaction.
    - Modified `main.ts` to configure session storage using PostgreSQL.
    - Updated `package.json` and `pnpm-lock.yaml` to reflect new dependencies and their versions.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
+import { Pool } from 'pg';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,8 +20,22 @@ async function bootstrap() {
     throw new Error('SESSION_COOKIE_NAME is not set');
   }
 
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+  }
+
+  const PgSession = connectPgSimple(session);
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
   app.use(
     session({
+      store: new PgSession({
+        pool,
+        tableName: 'session',
+        createTableIfMissing: true,
+      }),
       name: process.env.SESSION_COOKIE_NAME,
       secret: process.env.SESSION_SECRET,
       resave: false,
